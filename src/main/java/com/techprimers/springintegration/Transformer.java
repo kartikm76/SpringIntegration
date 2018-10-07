51642e6ee70e8b8a0063e2eb62c9eff2b0e460e6
kartikm76 <kartikm76@yahoo.com>
2018-10-07 22:27:44 +0530

    First Commit

---
+++ src/main/java/com/techprimers/springintegration/Transformer.java
@@ -0,0 +1,16 @@
+package com.techprimers.springintegration;
+
+import org.springframework.stereotype.Component;
+
+import java.io.IOError;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
+@Component
+public class Transformer {
+    public String transform(String filePath) throws IOException{
+        String content = new String(Files.readAllBytes(Paths.get(filePath)));
+        return "Tranformed " + content;
+    }
+}
